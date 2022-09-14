977713ad6997abe48c48db7623ff6eab8dbc69f6
Dominik Boehler <dominik.boehler@gmx.net>
2022-09-14 10:49:55 +0200

    update

--- darwin/bootstrap.nix
+++ darwin/bootstrap.nix
@@ -2,33 +2,34 @@
 
 {
   # Nix configuration ------------------------------------------------------------------------------
-
-  nix.binaryCaches = [
+nix.settings = {
+  substituters = [
     "https://cache.nixos.org/"
     "https://malo.cachix.org"
   ];
-  nix.binaryCachePublicKeys = [
+  trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "malo.cachix.org-1:fJL4+lpyMs/1cdZ23nPQXArGj8AS7x9U67O8rMkkMIo="
   ];
-  nix.trustedUsers = [
+  trusted-users = [
     "@admin"
   ];
-  users.nix.configureBuildUsers = true;
-
   # Enable experimental nix command and flakes
   # nix.package = pkgs.nixUnstable;
-  nix.extraOptions = ''
-    auto-optimise-store = true
-    experimental-features = nix-command flakes
-    keep-outputs = true
-    keep-derivations = true
-  '' + lib.optionalString (pkgs.system == "aarch64-darwin") ''
-    extra-platforms = x86_64-darwin aarch64-darwin
-  '';
 
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
+  auto-optimise-store = true;
+  experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+  keep-outputs = true;
+  keep-derivations = true;
+  extra-platforms = lib.mkIf (pkgs.system == "aarch64-darwin") [ "x86_64-darwin" "aarch64-darwin" ];
+};
+nix.configureBuildUsers = true;
+
+# Auto upgrade nix package and the daemon service.
+services.nix-daemon.enable = true;
 
 
   # Shells -----------------------------------------------------------------------------------------
