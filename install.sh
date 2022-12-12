ffd14efdffa27c85b097b92c1f5feff823a33ebb
Dominik Boehler <dominik.boehler@gmx.net>
2022-12-12 14:01:58 +0100

    added hack to put nix-profile on path

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 nix build .#homeConfigurations.malo.activationPackage
+sudo mkdir /home/dominikb1888
+sudo ln -s /home/code/.nix-profile /home/dominikb1888/.nix-profile
 ./result/activate
 
