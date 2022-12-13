f8fc7156a58d31e23b39e0d74af172de6c08a717
Dominik Boehler <dominik.boehler@gmx.net>
2022-12-13 16:50:13 +0100

    added exec fish to install .sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ nix build .#homeConfigurations.malo.activationPackage
 sudo mkdir /home/dominikb1888
 sudo ln -s /home/code/.nix-profile /home/dominikb1888/.nix-profile
 ./result/activate
-
+exec fish
