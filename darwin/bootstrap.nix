2169936a60e1fb78d0a425265eb9908b643a5a73
Dominik Boehler <dominik.boehler@gmx.net>
2023-03-16 13:41:30 +0100

    added own cachix url

--- darwin/bootstrap.nix
+++ darwin/bootstrap.nix
@@ -6,10 +6,12 @@
   nix.settings = {
     substituters = [
       "https://cache.nixos.org/"
+      "https://dominikb1888.cachix.org"
       "https://malo.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "dominikb1888.cachix.org-1:T7N2BXqaHQDDy2VXsQfA15WIyCLu/1eodq/BKxUPaQg="
       "malo.cachix.org-1:fJL4+lpyMs/1cdZ23nPQXArGj8AS7x9U67O8rMkkMIo="
     ];
 
