fb99d4d7ae79905b089076f38a7d1a87fcd5872b
Dominik Boehler <dominik.boehler@gmx.net>
2023-03-20 13:35:08 +0100

    added lorri via nix darwin

--- darwin/bootstrap.nix
+++ darwin/bootstrap.nix
@@ -32,7 +32,7 @@
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-
+  services.lorri.enable = true;
 
   # Shells -----------------------------------------------------------------------------------------
 
