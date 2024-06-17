a0fa4e28572797b071320ebd550cd62e094656f3
Dominik Boehler <dominik.boehler@gmx.net>
2024-06-17 09:02:55 +0200

    updates

--- darwin/debloat.sh
+++ darwin/debloat.sh
@@ -11,29 +11,52 @@
 TODISABLE=()
 
 TODISABLE+=('com.apple.accessibility.MotionTrackingAgent' \
-'com.apple.AddressBook.ContactsAccountsService' \
 'com.apple.AMPArtworkAgent' \
 'com.apple.AMPDeviceDiscoveryAgent' \
 'com.apple.AMPLibraryAgent' \
-'com.apple.ap.adprivacyd' \
-'com.apple.ap.adservicesd' \
-'com.apple.ap.promotedcontentd' \
-'com.apple.assistant_service' \
-'com.apple.assistantd' \
-'com.apple.avconferenced' \
+'com.apple.AddressBook.ContactsAccountsService' \
+'com.apple.AddressBook.abd' \
 'com.apple.BiomeAgent' \
-'com.apple.biomesyncd' \
 'com.apple.CalendarAgent' \
-'com.apple.calaccessd' \
 'com.apple.CallHistoryPluginHelper' \
-'com.apple.cloudd' \
-'com.apple.cloudpaird' \
-'com.apple.cloudphotod' \
 'com.apple.CloudPhotosConfiguration' \
 'com.apple.CloudSettingsSyncAgent' \
 'com.apple.CommCenter-osx' \
 'com.apple.ContactsAgent' \
 'com.apple.CoreLocationAgent' \
+'com.apple.DiagnosticReportCleanup.plist' \
+'com.apple.ManagedClient.cloudconfigurationd' \
+'com.apple.ManagedClientAgent.enrollagent' \
+'com.apple.Maps.mapspushd' \
+'com.apple.Maps.pushdaemon' \
+'com.apple.ReportCrash' \
+'com.apple.ReportCrash.Self' \
+'com.apple.ReportPanic' \
+'com.apple.Safari.SafeBrowsing.Service' \
+'com.apple.SafariCloudHistoryPushAgent' \
+'com.apple.SafariHistoryServiceAgent' \
+'com.apple.SafariLaunchAgent' \
+'com.apple.SafariNotificationAgent' \
+'com.apple.SafariPlugInUpdateNotifier' \
+'com.apple.ScreenTimeAgent' \
+'com.apple.Siri.agent' \
+'com.apple.TMHelperAgent' \
+'com.apple.TMHelperAgent.SetupOffer' \
+'com.apple.TrustEvaluationAgent' \
+'com.apple.UsageTrackingAgent' \
+'com.apple.WiFiVelocityAgent' \
+'com.apple.ap.adprivacyd' \
+'com.apple.ap.adservicesd' \
+'com.apple.ap.promotedcontentd' \
+'com.apple.appstoreagent' \
+'com.apple.assistant_service' \
+'com.apple.assistantd' \
+'com.apple.avconferenced' \
+'com.apple.biomesyncd' \
+'com.apple.calaccessd' \
+'com.apple.cloudd' \
+'com.apple.cloudpaird' \
+'com.apple.cloudphotod' \
 'com.apple.dataaccess.dataaccessd' \
 'com.apple.ensemble' \
 'com.apple.familycircled' \
@@ -44,10 +67,12 @@ TODISABLE+=('com.apple.accessibility.MotionTrackingAgent' \
 'com.apple.gamed' \
 'com.apple.geod' \
 'com.apple.geodMachServiceBridge' \
+'com.apple.helpd' \
 'com.apple.homed' \
-'com.apple.icloud.fmfd' \
 'com.apple.iCloudNotificationAgent' \
 'com.apple.iCloudUserNotifications' \
+'com.apple.iTunesHelper.launcher' \
+'com.apple.icloud.fmfd' \
 'com.apple.icloud.searchpartyuseragent' \
 'com.apple.imagent' \
 'com.apple.imautomatichistorydeletionagent' \
@@ -55,14 +80,12 @@ TODISABLE+=('com.apple.accessibility.MotionTrackingAgent' \
 'com.apple.intelligenceplatformd' \
 'com.apple.itunescloudd' \
 'com.apple.knowledge-agent' \
-'com.apple.ManagedClient.cloudconfigurationd' \
-'com.apple.ManagedClientAgent.enrollagent' \
-'com.apple.Maps.mapspushd' \
-'com.apple.Maps.pushdaemon' \
+'com.apple.macos.studentd' \
 'com.apple.mediaanalysisd' \
+'com.apple.mediastream.mstreamd' \
+'com.apple.mobiledeviceupdater' \
 'com.apple.networkserviceproxy' \
 'com.apple.networkserviceproxy-osx' \
-'com.apple.mediastream.mstreamd' \
 'com.apple.newsd' \
 'com.apple.nsurlsessiond' \
 'com.apple.parsec-fbf' \
@@ -73,40 +96,43 @@ TODISABLE+=('com.apple.accessibility.MotionTrackingAgent' \
 'com.apple.progressd' \
 'com.apple.protectedcloudstorage.protectedcloudkeysyncing' \
 'com.apple.quicklook' \
-'com.apple.quicklook.ui.helper' \
 'com.apple.quicklook.ThumbnailsAgent' \
+'com.apple.quicklook.ui.helper' \
 'com.apple.rapportd-user' \
 'com.apple.remindd' \
 'com.apple.routined' \
-'com.apple.SafariCloudHistoryPushAgent' \
+'com.apple.safaridavclient' \
+'com.apple.screensharing.MessagesAgent' \
 'com.apple.screensharing.agent' \
 'com.apple.screensharing.menuextra' \
-'com.apple.screensharing.MessagesAgent' \
-'com.apple.ScreenTimeAgent' \
 'com.apple.security.cloudkeychainproxy3' \
 'com.apple.sharingd' \
 'com.apple.sidecar-hid-relay' \
 'com.apple.sidecar-relay' \
-'com.apple.Siri.agent' \
 'com.apple.siri.context.service' \
-'com.apple.macos.studentd' \
 'com.apple.siriknowledged' \
+'com.apple.softwareupdate_notify_agent' \
 'com.apple.suggestd' \
-'com.apple.tipsd' \
 'com.apple.telephonyutilities.callservicesd' \
-'com.apple.TMHelperAgent' \
-'com.apple.TMHelperAgent.SetupOffer' \
+'com.apple.tipsd' \
 'com.apple.triald' \
 'com.apple.universalaccessd' \
-'com.apple.UsageTrackingAgent' \
 'com.apple.videosubscriptionsd' \
-'com.apple.WiFiVelocityAgent' \
 'com.apple.weatherd')
 
 for agent in "${TODISABLE[@]}"
 do
-	launchctl bootout gui/501/${agent}
-	launchctl disable gui/501/${agent}
+
+	[ -f /System/Library/LaunchDaemons/${daemon}.plist ] && {
+ 	echo "- Daemon ${daemon} exists, disabling"
+        sudo launchctl unload -wF /System/Library/LaunchDaemons/${daemon}.plist
+        launchctl unload -wF /System/Library/LaunchDaemons/${daemon}.plist
+	      mv /System/Library/LaunchDaemons/${daemon}.plist ~/DisabledLaunchDaemons/${daemon}.plist
+	echo "[OK] Daemon ${daemon} disabled"
+  } || {
+ 	echo "! Daemon ${daemon} does not exist"
+  }
+
 done
 
 launchctl unload -w /System/Library/LaunchAgents/com.apple.cloudpaird.plist
@@ -150,6 +176,13 @@ TODISABLE+=('com.apple.bootpd' \
 
 for daemon in "${TODISABLE[@]}"
 do
-	sudo launchctl bootout system/${daemon}
-	sudo launchctl disable system/${daemon}
+	[ -f /System/Library/LaunchDaemons/${daemon}.plist ] && {
+ 	echo "- Daemon ${daemon} exists, disabling"
+        sudo launchctl unload -wF /System/Library/LaunchDaemons/${daemon}.plist
+        launchctl unload -wF /System/Library/LaunchDaemons/${daemon}.plist
+	      mv /System/Library/LaunchDaemons/${daemon}.plist ~/DisabledLaunchDaemons/${daemon}.plist
+	echo "[OK] Daemon ${daemon} disabled"
+  } || {
+ 	echo "! Daemon ${daemon} does not exist"
+  }
 done
