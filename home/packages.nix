ed2c79782a74ab24032f58073f26a98f64290745
Dominik Böhler <dominik.boehler@gmx.net>
2026-08-17 06:36:04 +0200

    updates

--- home/packages.nix
+++ home/packages.nix
@@ -42,7 +42,7 @@ programs.ssh = {
   enable = true;
   enableDefaultConfig = false;
 
-  matchBlocks."*" = {
+  settings."*" = {
     forwardAgent = false;
     forwardX11 = false;
     serverAliveInterval = 30;
